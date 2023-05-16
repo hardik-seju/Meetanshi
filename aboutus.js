d4162b60b96b011f1feff142a459505455dead3e
seju-hardik <sejuhardik4@gmail.com>
2023-05-16 12:31:52 +0530

    axid psd to html

---
+++ aboutus.js
@@ -0,0 +1,37 @@
+function changecontent()
+{
+    var myImg = document.getElementById("changeimg");
+    var myText = document.getElementById("tabh2");
+    var myText1 = document.getElementById("tabp1");
+    var myText2 = document.getElementById("tabp2");
+    var color1 = document.getElementById("tab1");
+    myImg.src = 'img/building.png';
+    myImg.alt = "New Image";
+    myText.innerHTML = "Tabs With Soft Transitioning Effect";
+    myText1.innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores temporibus fugiat odio expeditaperferendis odit perspiciatis soluta totam dignissimos, consequuntur reprehenderit eligendiexercitationem enim quidem excepturi repellat minus fugit sunt eos? Corrupti ratione Asperiores Lorem, ipsum dolor";
+    myText2.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deserunt accusamus at ipsa? Iurj alias.";
+}
+function changecontent1() {
+    console.log("working mode on");
+    var myImg = document.getElementById("changeimg");
+    var myText = document.getElementById("tabh2");
+    var myText1 = document.getElementById("tabp1");
+    var myText2 = document.getElementById("tabp2");
+    myImg.src = 'img/change2.jpg';
+    myImg.alt = "New Image";
+    myText.innerHTML = "About The Computer";
+    myText1.innerHTML = "A computer is a machine that can be programmed to carry out sequences of arithmetic or logical operations automatically. Modern digital electronic computers can perform generic sets of operations known as programs. These programs enable computers to perform a wide range of tasks";
+    myText2.innerHTML = "The literal meaning of computer is a device that can calculate. However, modern computers can do a lot more than calculate. Computer is an electronic device .";
+  }
+  function changecontent2() {
+    console.log("working mode on");
+    var myImg = document.getElementById("changeimg");
+    var myText = document.getElementById("tabh2");
+    var myText1 = document.getElementById("tabp1");
+    var myText2 = document.getElementById("tabp2");
+    myImg.src = 'img/change3.jpg';
+    myImg.alt = "New Image";
+    myText.innerHTML = "Charles Babbage";
+    myText1.innerHTML = "The first computers were used primarily for numerical calculations. However, as any information can be numerically encoded, people soon realized that computers are capable of general-purpose information processing. Their capacity to handle large amounts of data has extended the range and accuracy of weather forecasting";
+    myText2.innerHTML = "Charles Babbage, originated the concept of a , he conceptualized and invented the first mechanical computer in the early 19th century.";
+  }
